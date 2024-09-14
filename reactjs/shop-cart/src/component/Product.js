1bb1aa63133505cde940a4bdab21ac9fdd5ca4e2
shatrudhankr5 <shatrudhankr5@gmail.com>
2024-09-14 13:12:30 +0530

    Added jspn-server

--- reactjs/shop-cart/src/component/Product.js
+++ reactjs/shop-cart/src/component/Product.js
@@ -2,28 +2,34 @@ import React, { useEffect, useState } from 'react'
 
 export const Product = () => {
   const [products, setProducts] = useState([]);
+  const [url, setUrl] = useState("http://localhost:4200/products");
+  const getProduct=async ()=>
+ {
+   const response= await fetch(url);
+   const data=await response.json();
+   setProducts(data);
+ } 
 
-  useEffect(() => {
+  useEffect((url) => {
+    getProduct();
 
-    fetch('http://localhost:4200/products')
-      .then(response => response.json())
-      .then(data => setProducts(data));
+  }, [url]);
 
-  }
-    , []);
 
   return (
     <section>
-    { products.map((product) => (
+      <button onClick={()=>setUrl('http://localhost:4200/products') }>All</button>
+      <button onClick={()=>setUrl('http://localhost:4200/products?in_stock=true') }>In Stock</button>
+      {products.map((product) => (
         <div className="card" key={product.id}>
-            <p className="id">{product.id}</p>
-            <p className="name">{product.name}</p>
-            <p className="info">
-                <span>${product.price}</span>
-                <span className={product.in_stock ? "instock" : "unavailable"}>{product.in_stock ? "In Stock" : "Unavailable"}</span>
-            </p>
-        </div>            
-    )) }
-</section>
+          <p className="id">{product.id}</p>
+          <p className="name">{product.name}</p>
+          <p className="info">
+            <span>${product.price}</span>
+            <span className={product.in_stock ? "instock" : "unavailable"}>{product.in_stock ? "In Stock" : "Unavailable"}</span>
+          </p>
+        </div>
+      ))}
+    </section>
   )
 }
